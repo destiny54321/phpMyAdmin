918582727a1ff541130c1b7d9b42544256585514
destiny54321 <bk.bharat88@gmail.com>
2021-03-30 18:10:20 +0530

    Update config.sample.inc.php

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'ipl';
+$cfg['Servers'][$i]['host'] = 'php-my-admin';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
